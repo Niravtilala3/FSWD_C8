0506e1209134b7a55da9df91ad7d405cb1bb4276
Nirav Tilala <niravtilala3@gmail.com>
2026-06-25 07:51:23 +0530

    Add POST route for homepage to handle submissions

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/', (req, res) => {
+  res.send('Post Request to the homepage');
+});
+
 app.use('/user', userRoute);
 app.use('/emp', empRoute);
 
