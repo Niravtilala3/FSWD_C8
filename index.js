ed9ac6805f0e774a5175c75422bc1ee8ba1167e1
Nirav Tilala <niravtilala3@gmail.com>
2026-06-29 07:20:08 +0530

    Refactor routes and update view engine to EJS; add user and employee routes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const userRoute = require('./route/userRoute');
-const empRoute = require('./route/empRoute');
+const userRoute = require('./routes/userRoute');
+const empRoute = require('./routes/empRoute');
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const message = {name: 'Nirav', age: 30};
+  res.render('index', { title: 'Hey', message: message });
   // res.send('Hello World!');
 });
 
@@ -20,5 +21,5 @@ app.use('/user', userRoute);
 app.use('/emp', empRoute);
 
 app.listen(3000,()=>{
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port http://localhost:3000');
 });
