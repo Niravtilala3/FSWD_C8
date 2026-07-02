6657466cce8505622af8c4e1bf32212222d588dd
Nirav Tilala <niravtilala3@gmail.com>
2026-07-02 08:22:03 +0530

    Implement user CRUD operations, add error handling, and create 404 page; update index view and styles

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const empRoute = require('./routes/empRoute');
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.json());
 app.use(express.static('public'));
 
 const requestTime = function (req, res, next) {
@@ -15,12 +16,13 @@ const requestTime = function (req, res, next) {
 
 app.use(requestTime);
 
-
 app.get('/', (req, res) => {
-
-  
-  const message = {name: 'Nirav', age: 30};
-  res.render('index', { title: 'Hey', message: message });
+  console.log('req.hostname', req.hostname);
+  console.log('req.ip', req.ip);
+  console.log('req.path', req.path);
+  console.log('req.method', req.method);
+  console.log('req.protocol', req.protocol);
+  res.render('index', { title: 'Hey', message: {name: 'Nirav', age: 30} });
   // res.send('Hello World!');
 });
 
@@ -28,8 +30,22 @@ app.post('/', (req, res) => {
   res.send('Post Request to the homepage');
 });
 
-app.use('/appuser', userRoute);
+app.use('/user', userRoute);
 app.use('/emp', empRoute);
+
+app.get('/error', (req, res) => {
+  throw new Error('BROKEN');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  //
+  res.status(500).send('Something broke!');
+});
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { title: '404 Not Found' });
+});
 
 app.listen(3000,()=>{
   console.log('Server is running on port http://localhost:3000');
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,52 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+
+const users= [
+  { id: 1, name: 'John Doe', city: 'Rajkot'},
+  { id:2, name: 'raj', city :'Rajkot '},
+]
+
 
 router.get('/',(req, res)=>{
-  res.send('user home page');
+  res.json(users);
 })
+
+router.get('/:usrId',(req, res)=>{
+  const user = users.find(u => u.id === parseInt(req.params.usrId));
+  if (!user){
+    return res.status(404).json({ error: 'User not found' });
+  }
+  res.json(user);
+})
+
+router.post('/',(req, res)=>{
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+    city: req.body.city
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+router.put('/:usrId',(req, res)=>{
+  const user = users.find(u => u.id === parseInt(req.params.usrId));
+  if (!user){
+    return res.status(404).json({ error: 'User not found' });
+  }
+  if(req.body && req.body.name) user.name = req.body.name;
+  if(req.body && req.body.city) user.city = req.body.city ;
+  res.json(user);
+});
+
+router.delete('/:usrId',(req, res)=>{
+  const userIndex = users.findIndex(u => u.id === parseInt(req.params.usrId));
+  if (userIndex === -1){
+    return res.status(404).json({ error: 'User not found' });
+  }
+  const deletedUser = users.splice(userIndex, 1);
+  res.json(deletedUser[0]);
+});
 
 router.get('/info',(req, res)=>{
   res.send('user info page');
